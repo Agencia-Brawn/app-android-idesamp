b593536eb54f4b4fbb7afd4e8c44a69bd45f8b61
Anderson Coimbra <andersonmegax@hotmail.com>
2021-11-19 03:11:32 -0300

    Correção de appID e Nome

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Amandaba da Amazônia',
+      title: 'Botão de Resgate',
       theme: ThemeData(
         // This is the theme of your application.
         //
